073563973bd6dbfff8425c61615cc747025e63d8
KevinDavai <kevin.davailles@etu.uca.fr>
2022-07-04 22:51:00 +0200

    Add helpers function for mongodb

--- src/Events/Client/ready.ts
+++ src/Events/Client/ready.ts
@@ -1,6 +1,7 @@
 import { Client } from 'discord.js';
 import * as pokemon from '../../Helpers/pokemon';
 import { createGuildDataOfflined, deleteGuildDataOfflined } from '../../Database/UtilsModals/UtilsGuilds';
+import { insertDataInExistingDoc } from '../../Helpers/mongo';
 
 export default {
     name: "ready",
@@ -16,6 +17,8 @@ export default {
         // create and delete guilds that were added or deleted when the bot was offline
         createGuildDataOfflined(client)
         deleteGuildDataOfflined(client)
+
+        insertDataInExistingDoc("guilds", {test: "Success"})
 
         // TODO: Make a function "main" for this and make editable variable for the Interval time
         setInterval(() => {
--- src/Helpers/mongo.ts
+++ src/Helpers/mongo.ts
@@ -1,9 +1,26 @@
-import mongoose from "mongoose";
+import mongoose, { Model, model } from 'mongoose';
+import { Interface } from 'readline';
+import { TextEncoderStream } from 'stream/web';
+import { Guilds } from '../Database/Modals/guildsModal';
 
+/**
+ * Database connection
+ */
 export const connectDatabase = async (): Promise<void> => {
     mongoose.connect(process.env.MONGO_URI).then(() => {
         console.log('Connected to MongoDB')
     }).catch((err) => {
-        console.log('Unable to connect to MongoDB Database.\nError: ' + err)
+        console.error('Unable to connect to MongoDB Database.\nError: ' + err)
     })
+}
+
+/**
+ * Insert data to all document of a given collection
+ * USE THIS ONLY AFTER UPDATE THE SCHEMA
+ * @param modelName 
+ * @param obj  
+ */
+export const insertDataInExistingDoc = async (modelName: string, obj: object): Promise<void> => {
+    const collection  = mongoose.connection.db.collection(modelName);
+    collection.updateMany({ },{ $set: obj });
 }
